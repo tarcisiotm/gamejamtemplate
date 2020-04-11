5ce1523b2e01dd9a9fabe5ba06b38be2e73cc822
Tarcísio Costa <tarcisiotm@gmail.com>
2020-04-10 21:39:28 -0700

    UI

---
+++ Assets/Scripts/Audio/AudioButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class AudioButton : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip onHover;
+    [SerializeField] float onHoverVolume = .6f;
+    [SerializeField] AudioClip clickClip;
+
+    private void Awake()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    public void OnClick()
+    {
+        audioSource.volume = 1f;
+        audioSource.PlayOneShot(clickClip);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        audioSource.volume = onHoverVolume;
+        audioSource.PlayOneShot(onHover);
+    }
+}
