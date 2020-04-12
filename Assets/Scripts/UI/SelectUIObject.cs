01ee63c585c3e8fd4d756d3783bb77e9cae3a390
Tarcísio Costa <tarcisiotm@gmail.com>
2020-04-11 19:15:02 -0700

    -UI Select / Highlight

---
+++ Assets/Scripts/UI/SelectUIObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Component used to select a GameObject for highlighting UI
+/// </summary>
+public class SelectUIObject : MonoBehaviour
+{
+    [SerializeField] bool onEnable = true;
+    [SerializeField] GameObject gameObjectToSelect;
+
+    private void OnEnable() {
+        if (!onEnable) { return; }
+        SelectGameObject();
+    }
+
+    public void SelectGameObject() {
+        if(gameObjectToSelect == null) {
+            Debug.LogWarning("Null GameObject Selected");
+            return;
+        }
+
+        EventSystem.current.SetSelectedGameObject(gameObjectToSelect);
+    }
+}
