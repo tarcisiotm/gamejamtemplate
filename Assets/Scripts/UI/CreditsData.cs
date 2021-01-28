bf45c3af322ed52937b1f7a1daba9df13d8ae013
Tarcísio Costa <tarcisiotm@gmail.com>
2021-01-27 19:12:11 -0800

    Credits System

---
+++ Assets/Scripts/UI/CreditsData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Credits", menuName = "Data/Create Credits Asset")]
+public class CreditsData : ScriptableObject
+{
+    [SerializeField] string _title;
+    [SerializeField] CreditsEntryData[] _creditsEntries;
+
+    public string Title => _title;
+    public CreditsEntryData[] CreditsEntries => _creditsEntries;
+
+}
+
+[System.Serializable]
+public class CreditsEntryData
+{
+    public string Name;
+    public string[] Links;
+}
