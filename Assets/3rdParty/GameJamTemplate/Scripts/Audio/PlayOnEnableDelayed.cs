ad929f9eb0d91df382034fcf144a5bf7d1a9710e
Tarcísio Costa <tarcisiotm@gmail.com>
2021-05-10 19:25:57 -0700

    New folder structure

---
+++ Assets/3rdParty/GameJamTemplate/Scripts/Audio/PlayOnEnableDelayed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A simmple component to delay playing an audio on enable
+/// </summary>
+public class PlayOnEnableDelayed : MonoBehaviour
+{
+    [SerializeField] float delay = 0;
+
+    Coroutine WaitRoutine;
+    private void OnEnable() {
+        WaitRoutine = StartCoroutine(DelayToPlay());
+    }
+
+    IEnumerator DelayToPlay() {
+        yield return new WaitForSeconds(delay);
+        GetComponent<AudioSource>().Play();
+        WaitRoutine = null;
+    }
+
+    private void OnDisable() {
+        if(WaitRoutine != null) {
+            StopCoroutine(WaitRoutine);
+        }
+    }
+}
---
+++ Assets/3rdParty/GameJamTemplate/Scripts/Managers/GameplayManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TG.Core;
+using UnityEngine;
+
+/// <summary>
+/// Handles gameplay functionality
+/// </summary>
+public class GameplayManager : Singleton<GameplayManager>
+{
+    [Header("References")]
+    [SerializeField] HUDManager hudManager = default;
+
+    [SerializeField] Camera mainCamera = default;
+
+    PoolingController poolingController = default;
+    GameObject auxGO = default;
+
+    void Start()
+    {
+    }
+
+    public void CreateObjectFromPool(GameObject prefab, Vector3 pos) {
+        auxGO = poolingController.GetPooledObject(prefab);
+        auxGO.transform.position = pos;
+        auxGO.SetActive(true);
+    }
+
+    public void ParentToCamera(Transform newChild) {
+        newChild.SetParent(mainCamera.transform, true);
+    }
+
+    #region Player
+    //public Vector3 GetPlayerPos() {
+        //return Player.transform.position;
+    //}
+
+    IEnumerator DoRespawn() {
+        yield return new WaitForSeconds(3);
+        //Player.gameObject.SetActive(true);
+        //Player.Respawn(pilot.transform);
+    }
+
+    #endregion Player
+
+    #region HUD
+    public void IncrementScore(int points) {
+        //Score += points;
+        //hudManager.UpdateScore(Score);
+    }
+
+    void LoseLife() {
+        hudManager.LoseLife();
+    }
+    #endregion HUD
+
+    public void GameOver() {
+        hudManager.Save();
+        StartCoroutine(DelayGameOver());
+    }
+
+    IEnumerator DelayGameOver() {
+        yield return new WaitForSeconds(1);
+        hudManager.ShowGameOver();
+    }
+
+}
